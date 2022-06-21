9cb1d6f630ac5f640d644adf10f7813f6731486b
unknown <pnvyas1327@gmail.com>
2022-06-21 11:27:03 +0530

    new hystrix-dashboard service repo created

---
+++ hystrix-dashboard/src/main/java/com/apidesign/hystrix/dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.apidesign.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+
+@EnableHystrixDashboard
+@EnableEurekaClient
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+}
